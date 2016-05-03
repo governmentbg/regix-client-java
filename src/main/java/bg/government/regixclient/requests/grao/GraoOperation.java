a51ae9270fb7783a6141e91db0091c5ee16492b5
bozhanov <bozhidar.bozhanov@tomtom.com>
2016-05-03 23:52:23 +0300

    commercial register classes added

--- src/main/java/bg/government/regixclient/requests/grao/GraoOperation.java
+++ src/main/java/bg/government/regixclient/requests/grao/GraoOperation.java
@@ -4,7 +4,7 @@ import bg.government.regixclient.requests.Operation;
 
 public enum GraoOperation implements Operation {
     PERMANENT_ADDRESS_SEARCH("TechnoLogica.RegiX.GraoPNAAdapter.APIService.IPNAAPI.PermanentAddressSearch"),
-    PERSON_DATA_SEARCH("echnoLogica.RegiX.GraoNBDAdapter.APIService.INBDAPI.PersonDataSearch ");
+    PERSON_DATA_SEARCH("TechnoLogica.RegiX.GraoNBDAdapter.APIService.INBDAPI.PersonDataSearch");
     
     private final String key;
     
